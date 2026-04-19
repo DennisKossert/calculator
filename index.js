c1dcdd6dcdaaba19824bd4813769cb4640737ba9
DennisKossert <dennis@kossert.org>
2026-04-19 14:10:31 +0200

    Add basic calculation logic

--- index.js
+++ index.js
@@ -15,4 +15,34 @@ function divide (a, b) {
     //TODO: decimals
 }
 
-const operator = /[+\-*/]/
+// const operatorReg = /[+\-*/]/;
+// ^ might need this in the future to identify ANY operator in input strings (if I want to have multiple operators possible for example)
+
+function calculate (a, b, operator) {
+    switch (operator) {
+
+        case "+" :
+            return add(a, b);
+
+        case "-" :
+            return subtract(a, b);
+
+        case "*" :
+            return multiply(a, b);
+        
+        case "/" :
+            return divide(a, b);
+    }
+}
+
+// DEBUG FUNCTION - ignore
+
+function debug(arr) {
+    let i = 0;
+    while (i < arr.length) {
+        if (operator.test(arr[i])) {console.log(arr[i])}
+        i++;
+    }
+}
+
+// debug(["hello+world","boing",7,(6+6),"/","-rip","***"]); 
